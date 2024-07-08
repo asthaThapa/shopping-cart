0bffcaacd3adc79f3970c1bd9d7f6b9097422a86
asthaThapa <aastha0914@gmail.com>
2024-07-08 17:50:52 -0400

    Boiler Plate Code

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Home } from '../pages/Home';
+export default (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
+)
